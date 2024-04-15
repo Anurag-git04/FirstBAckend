cd1d645fce3fe2b917161bd36845f95807c9bb19
Anurag-git04 <shawanurag18@gmail.com>
2024-04-15 23:37:48 +0530

    3rd

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-
-
-import mongoose from "mongoose"
-import {DB_NAME} from "./constant"
-import connectDB from "./db";
-
+import connectDB from "./db/index.js";
 dotenv.config({
-    path:"./env"
+    path: './.env'
 })
+
 
 
 connectDB()
